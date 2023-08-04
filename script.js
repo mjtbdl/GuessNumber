db626f3d5cd4194a988ee993572b1a2d94024a37
Mecit Bedel <mjt.bdl@gmail.com>
2023-08-04 15:25:14 +0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 'use strict';
+
+let value = Math.trunc(Math.random() * 20) + 1;
+
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
+
+function correctNumber() {
+  document.querySelector('.message').textContent = 'Correct Number';
+  document.querySelector('body').style.backgroundColor = '#60b347';
+  document.querySelector('.number').textContent = value;
+  document.querySelector('.number').style.width = '30rem';
+}
+
+function newRound() {
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = 5;
+  value = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number';
+  } else if (guess === value) {
+    correctNumber();
+    if (
+      document.querySelector('.score').textContent >
+      document.querySelector('.highscore').textContent
+    ) {
+      document.querySelector('.highscore').textContent =
+        document.querySelector('.score').textContent;
+    }
+  } else if (guess > value) {
+    document.querySelector('.message').textContent = 'Too high';
+    document.querySelector('.score').textContent--;
+  } else if (guess < value) {
+    document.querySelector('.score').textContent--;
+    document.querySelector('.message').textContent = 'Too low';
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  newRound();
+});
